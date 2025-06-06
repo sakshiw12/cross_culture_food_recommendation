5b9249aa0d95356190862cd2682e6cc54de6ef4a
sakshiw12 <sakshipw0207@gmail.com>
2025-06-06 11:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,8 @@ def predict():
     return render_template('index.html', recipes=df['name'].tolist(),
                            prediction_text="Recommended Dishes:", tables=[recommendations.to_html(classes='data', header="true", index=False)])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
